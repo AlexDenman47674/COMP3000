55d112febfd3694f8f784fdf7082e7da3a319ea5
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-12-01 09:48:57 +0000

    Added components to Login Frontend

--- App Pages/face-recognition-app/Login.tsx
+++ App Pages/face-recognition-app/Login.tsx
@@ -1,5 +1,19 @@
 import react from 'react'
 <div>
+<h2>Welcome</h2>
 
-
+<h3>Already have an account?</h3>
+<div>
+    <h3>Log In:</h3>
+    <label>
+        User Name
+        <input type="text" name="User Name" />
+        </label>
+    <label>
+        Password
+        <input type="text" password="Password" />
+    </label>
+    <h3>Don't have an account?</h3>
+    <button><h3>Sign Up</h3></button>;
+</div>
 </div>
---
+++ App Pages/face-recognition-app/SignUp.tsx
@@ -0,0 +1,5 @@
+import react from 'react'
+<div>
+
+
+</div>
