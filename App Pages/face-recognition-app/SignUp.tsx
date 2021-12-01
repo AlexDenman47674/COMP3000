2ac19b8895742da26abf6541c4301428fb393b42
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-12-01 11:38:08 +0000

    Added react components to sign up page

--- App Pages/face-recognition-app/SignUp.tsx
+++ App Pages/face-recognition-app/SignUp.tsx
@@ -1,5 +1,18 @@
 import react from 'react'
 <div>
+<h2>Welcome</h2>
 
+<div>
+    <h3>Sign Up</h3>
+    <label>
+        User Name
+        <input type="text" name="User Name" />
+        </label>
+    <label>
+        Password
+        <input type="text" password="Password" />
+    </label>
+    <button><h3>Get Started</h3></button>;
+</div>
 
 </div>
