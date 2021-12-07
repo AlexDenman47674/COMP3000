f0fe37543853b15304283dd7d5e6e94671848875
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-12-07 15:46:15 +0000

    Added CSS to password page

--- App Pages/face-recognition-app/Password.tsx
+++ App Pages/face-recognition-app/Password.tsx
@@ -1,4 +1,33 @@
 import react from 'react'
+
+function Untitled(props) {
+    return (
+      <Container>
+        <Rect></Rect>
+      </Container>
+    );
+  }
+  
+  const Container = `
+    display: flex;
+    background-color: rgba(114,189,235,1);
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+  `;
+  
+  const Rect = `
+    width: 330px;
+    height: 558px;
+    background-color: rgba(248,249,251,1);
+    border-radius: 30px;
+    margin-top: 39px;
+    align-self: center;
+  `;
+  
+  export default Untitled;
+
+
 <div>
 
 <label>
