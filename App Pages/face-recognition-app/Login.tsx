5ec07a9c27f8ae2c4a9283e8b32b3fcc9e1ed21d
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-12-07 12:22:16 +0000

    Added CSS to Login Page

--- App Pages/face-recognition-app/Login.tsx
+++ App Pages/face-recognition-app/Login.tsx
@@ -1,4 +1,108 @@
-import react from 'react'
+import React, { Component } from "react";
+
+
+function Untitled(props) {
+  return (
+    <Container>
+      <Rect>
+        <Welcome>Welcome</Welcome>
+        <LoremIpsum>Already have an account?</LoremIpsum>
+        <Rect2>
+          <LogIn>Log In:</LogIn>
+        </Rect2>
+        <LoremIpsum2>Don&#39;t have an account?</LoremIpsum2>
+        <SignUp>Sign Up</SignUp>
+      </Rect>
+    </Container>
+  );
+}
+
+const Container = `
+  display: flex;
+  background-color: rgba(114,189,235,1);
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Rect = `
+  width: 330px;
+  height: 558px;
+  background-color: rgba(248,249,251,1);
+  border-radius: 30px;
+  flex-direction: column;
+  display: flex;
+  margin-top: 40px;
+  align-self: center;
+`;
+
+const Welcome = `
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 40px;
+  margin-top: 18px;
+  margin-left: 83px;
+`;
+
+const LoremIpsum = `
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 20px;
+  margin-top: 87px;
+  margin-left: 52px;
+`;
+
+const Rect2 = `
+  width: 299px;
+  height: 180px;
+  background-color: rgba(255,251,251,1);
+  border-radius: 13px;
+  border-width: 1px;
+  border-color: #000000;
+  flex-direction: column;
+  display: flex;
+  margin-top: 27px;
+  align-self: center;
+  border-style: solid;
+`;
+
+const LogIn = `
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 20px;
+  margin-top: 9px;
+  margin-left: 6px;
+`;
+
+const LoremIpsum2 = `
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 20px;
+  margin-top: 91px;
+  align-self: center;
+`;
+
+const SignUp = `
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  color: #121212;
+  font-size: 30px;
+  margin-top: 13px;
+  margin-left: 114px;
+`;
+
+export default Untitled;
+
+
 <div>
 <h2>Welcome</h2>
 
