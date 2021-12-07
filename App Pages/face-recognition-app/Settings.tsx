6d8adccb95d6a7ed62d755e5f4fb6beb0f8bb43e
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-12-07 15:45:11 +0000

    Added CSS to settings page

--- App Pages/face-recognition-app/Settings.tsx
+++ App Pages/face-recognition-app/Settings.tsx
@@ -1,4 +1,46 @@
 import react from 'react'
+
+function Untitled(props) {
+    return (
+      <Container>
+        <Rect>
+          <LoggedInAsUser>Logged in as: [USER]</LoggedInAsUser>
+        </Rect>
+      </Container>
+    );
+  }
+  
+  const Container = `
+    display: flex;
+    background-color: rgba(114,189,235,1);
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+  `;
+  
+  const Rect = `
+    width: 330px;
+    height: 558px;
+    background-color: rgba(248,249,251,1);
+    border-radius: 30px;
+    flex-direction: column;
+    display: flex;
+    margin-top: 39px;
+    align-self: center;
+  `;
+  
+  const LoggedInAsUser = `
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    color: #121212;
+    margin-top: 11px;
+    margin-left: 102px;
+  `;
+  
+  export default Untitled;
+
+
 <div>
 <h3>Signed in as <h3 i>USER</h3></h3>
 
