14e0d68a8838c7f1686074efe929bb3b41486dd7
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-03-06 14:02:29 +0000

    Began setting up connection between cloud & backend

--- FacialRecognitionProject/FacialRecognitionProject/Form1.cs
+++ FacialRecognitionProject/FacialRecognitionProject/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Net;
+using MySqlConnector;
 
 namespace FacialRecognitionProject
 {
@@ -15,6 +12,23 @@ namespace FacialRecognitionProject
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var connectionString = new MySqlConnectionStringBuilder()
+            {
+                SslMode = MySqlSslMode.None,
+
+                Server = Environment.GetEnvironmentVariable("34.89.124.128"),   // e.g. '127.0.0.1'
+                // Set Host to 'cloudsql' when deploying to App Engine Flexible environment
+                UserID = Environment.GetEnvironmentVariable("adenman"),   // e.g. 'my-db-user'
+                Password = Environment.GetEnvironmentVariable("Treclaren01%"), // e.g. 'my-db-password'
+                Database = Environment.GetEnvironmentVariable("COMP3000"), // e.g. 'my-database'
+            };
+            connectionString.Pooling = true;
+            // Specify additional properties here.
+            //return connectionString;
         }
     }
 }
