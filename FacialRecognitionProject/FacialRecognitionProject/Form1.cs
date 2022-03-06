a93b94ae09f60ae4cff268999ce0a25cdc2367a2
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-03-06 15:21:12 +0000

    Continued attempt to connect database to backend

--- FacialRecognitionProject/FacialRecognitionProject/Form1.cs
+++ FacialRecognitionProject/FacialRecognitionProject/Form1.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Net;
 using MySqlConnector;
+using System.Web.Mvc;
 
 namespace FacialRecognitionProject
 {
@@ -14,21 +15,47 @@ namespace FacialRecognitionProject
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public class HomeController : Controller
+        {
+            readonly MySqlConnectionStringBuilder _connectionString;
+
+            public HomeController(
+                MySqlConnectionStringBuilder connectionString)
+            {
+                this._connectionString = connectionString;
+
+                try
+                {
+                    using (var connection = new MySqlConnection(_connectionString.ConnectionString))
+                    {
+                        connection.Open();
+                        MessageBox.Show("Connection Established");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error Occurred");
+                }
+
+            }
+
+
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
         {
             var connectionString = new MySqlConnectionStringBuilder()
             {
                 SslMode = MySqlSslMode.None,
 
                 Server = Environment.GetEnvironmentVariable("34.89.124.128"),   // e.g. '127.0.0.1'
-                // Set Host to 'cloudsql' when deploying to App Engine Flexible environment
                 UserID = Environment.GetEnvironmentVariable("adenman"),   // e.g. 'my-db-user'
                 Password = Environment.GetEnvironmentVariable("Treclaren01%"), // e.g. 'my-db-password'
                 Database = Environment.GetEnvironmentVariable("COMP3000"), // e.g. 'my-database'
             };
             connectionString.Pooling = true;
-            // Specify additional properties here.
-            //return connectionString;
+
+
         }
     }
 }
