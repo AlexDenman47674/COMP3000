720ff76cf752c21bf8c821d250320565541f9d60
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-03-07 22:50:29 +0000

    Created Database Form

---
+++ FacialRecognitionProject/FacialRecognitionProject/DatabaseForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace FacialRecognitionProject
+{
+    partial class DatabaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCurrentUser = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.databaseViewer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelCurrentUser
+            // 
+            this.labelCurrentUser.AutoSize = true;
+            this.labelCurrentUser.BackColor = System.Drawing.Color.Transparent;
+            this.labelCurrentUser.Font = new System.Drawing.Font("Myanmar Text", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentUser.Location = new System.Drawing.Point(888, 9);
+            this.labelCurrentUser.Name = "labelCurrentUser";
+            this.labelCurrentUser.Size = new System.Drawing.Size(66, 37);
+            this.labelCurrentUser.TabIndex = 5;
+            this.labelCurrentUser.Text = "USER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Myanmar Text", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(753, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Logged In As:";
+            // 
+            // databaseViewer
+            // 
+            this.databaseViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.databaseViewer.Location = new System.Drawing.Point(56, 59);
+            this.databaseViewer.Name = "databaseViewer";
+            this.databaseViewer.Size = new System.Drawing.Size(886, 464);
+            this.databaseViewer.TabIndex = 6;
+            // 
+            // DatabaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FacialRecognitionProject.Properties.Resources.MainBackground;
+            this.ClientSize = new System.Drawing.Size(1007, 806);
+            this.Controls.Add(this.databaseViewer);
+            this.Controls.Add(this.labelCurrentUser);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "DatabaseForm";
+            this.Text = "Database";
+            ((System.ComponentModel.ISupportInitialize)(this.databaseViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCurrentUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView databaseViewer;
+    }
+}
