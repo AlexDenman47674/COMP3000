a43aa498e6e67fbf224f950fe4ca1da982ab3d8f
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-04-15 13:22:15 +0100

    Created the Detect Face Method

--- FacialRecognitionProject/FacialRecognitionProject/AzureFace.cs
+++ FacialRecognitionProject/FacialRecognitionProject/AzureFace.cs
@@ -254,5 +254,25 @@ namespace FacialRecognitionProject
             }
             Console.WriteLine();
         }
+
+
+        private static async Task<List<DetectedFace>> DetectFaceRecognize(IFaceClient faceClient, string url, string recognition_model)
+        {
+            // Detect faces from image URL. Since only recognizing, use the recognition model 1.
+            // We use detection model 3 because we are not retrieving attributes.
+            IList<DetectedFace> detectedFaces = await faceClient.Face.DetectWithUrlAsync(url, recognitionModel: recognition_model, detectionModel: DetectionModel.Detection03, FaceAttributes: new List<FaceAttributeType> { FaceAttributeType.QualityForRecognition });
+            List<DetectedFace> sufficientQualityFaces = new List<DetectedFace>();
+            foreach (DetectedFace detectedFace in detectedFaces)
+            {
+                var faceQualityForRecognition = detectedFace.FaceAttributes.QualityForRecognition;
+                if (faceQualityForRecognition.HasValue && (faceQualityForRecognition.Value >= QualityForRecognition.Medium))
+                {
+                    sufficientQualityFaces.Add(detectedFace);
+                }
+            }
+            Console.WriteLine($"{detectedFaces.Count} face(s) with {sufficientQualityFaces.Count} having sufficient quality for recognition detected from image `{Path.GetFileName(url)}`");
+
+            return sufficientQualityFaces.ToList();
+        }
     }
 }
