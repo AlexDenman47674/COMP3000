7f3bb6220c0794312a55cd1c5cfba43c254678de
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-04-15 11:47:49 +0100

    Created new method for face identification

--- FacialRecognitionProject/FacialRecognitionProject/AzureFace.cs
+++ FacialRecognitionProject/FacialRecognitionProject/AzureFace.cs
@@ -159,7 +159,24 @@ namespace FacialRecognitionProject
             }
         }
 
+        public static async Task IdentifyInPersonGroup(IFaceClient client, string url, string recognitionModel)
+        {
+            Console.WriteLine("========IDENTIFY FACES========");
+            Console.WriteLine();
 
+            // Create a dictionary for all your images, grouping similar ones under the same key.
+            Dictionary<string, string[]> personDictionary =
+                new Dictionary<string, string[]>
+                    { { "Family1-Dad", new[] { "Family1-Dad1.jpg", "Family1-Dad2.jpg" } },
+              { "Family1-Mom", new[] { "Family1-Mom1.jpg", "Family1-Mom2.jpg" } },
+              { "Family1-Son", new[] { "Family1-Son1.jpg", "Family1-Son2.jpg" } },
+              { "Family1-Daughter", new[] { "Family1-Daughter1.jpg", "Family1-Daughter2.jpg" } },
+              { "Family2-Lady", new[] { "Family2-Lady1.jpg", "Family2-Lady2.jpg" } },
+              { "Family2-Man", new[] { "Family2-Man1.jpg", "Family2-Man2.jpg" } }
+                    };
+            // A group photo that includes some of the persons you seek to identify from your dictionary.
+            string sourceImageFileName = "identification1.jpg";
 
+        }
     }
 }
