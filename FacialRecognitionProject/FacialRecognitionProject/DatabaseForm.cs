e0804eaecf33f50cf71f8047beab88f67ae33284
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-04-20 12:38:58 +0100

    Created person class in Database form

--- FacialRecognitionProject/FacialRecognitionProject/DatabaseForm.cs
+++ FacialRecognitionProject/FacialRecognitionProject/DatabaseForm.cs
@@ -18,33 +18,44 @@ namespace FacialRecognitionProject
             InitializeComponent();
         }
 
+        public class Person
+        {
+            public int PersonID { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int UserID { get; set; }
+        }
+
         private void DatabaseForm_Load(object sender, EventArgs e)
         {
-            string FileName = "Database(People).xlsx";
-            //OleDbConnection ExcelConnection = null;
-            System.Data.OleDb.OleDbConnection ExcelConnection;
-            ExcelConnection = null;
-            string filePath = "C:/Users/Alex/Desktop/COMP3000/";
-            DataTable dtNew = new DataTable();
-            ExcelConnection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + "'" + filePath + FileName + "'" + ";Extended Properties=Excel 8.0;");
-            try
-            {
-                ExcelConnection.Open();
-                DataTable dt = ExcelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                System.Data.OleDb.OleDbCommand ExcelCommand = new OleDbCommand(@"SELECT * FROM [" + "Sheet1" + @"]", ExcelConnection);
-                OleDbDataAdapter ExcelAdapter = new OleDbDataAdapter(ExcelCommand);
-                DataSet ExcelDataSet = new DataSet();
-                ExcelAdapter.Fill(dataSet1);
-                ExcelConnection.Close();
 
-            }
-            catch (Exception ex)
-            {
 
-            }
-            finally
-            {
-            }
+
+            //string FileName = "Database(People).xlsx";
+            ////OleDbConnection ExcelConnection = null;
+            //System.Data.OleDb.OleDbConnection ExcelConnection;
+            //ExcelConnection = null;
+            //string filePath = "C:/Users/Alex/Desktop/COMP3000/";
+            //DataTable dtNew = new DataTable();
+            //ExcelConnection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + "'" + filePath + FileName + "'" + ";Extended Properties=Excel 8.0;");
+            //try
+            //{
+            //    ExcelConnection.Open();
+            //    DataTable dt = ExcelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            //    System.Data.OleDb.OleDbCommand ExcelCommand = new OleDbCommand(@"SELECT * FROM [" + "Sheet1" + @"]", ExcelConnection);
+            //    OleDbDataAdapter ExcelAdapter = new OleDbDataAdapter(ExcelCommand);
+            //    DataSet ExcelDataSet = new DataSet();
+            //    ExcelAdapter.Fill(dataSet1);
+            //    ExcelConnection.Close();
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+            //finally
+            //{
+            //}
         }
 
         private void databaseViewer_CellContentClick(object sender, DataGridViewCellEventArgs e)
