fb9a81aba76512a27ea683d0e6fad53ef89181c9
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2022-04-25 23:20:46 +0100

    Camera Form now reads People & Images JSON files

--- FacialRecognitionProject/FacialRecognitionProject/CameraForm.cs
+++ FacialRecognitionProject/FacialRecognitionProject/CameraForm.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,6 +17,24 @@ namespace FacialRecognitionProject
     {
 
         AzureFace FaceAI = new AzureFace();
+        List<Person> DBPeople;
+        List<Images> DBImages;
+
+        public class Person
+        {
+            public int PersonID { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int UserID { get; set; }
+        }
+
+        public class Images
+        {
+            public int ImageID { get; set; }
+            public string ImageName { get; set; }
+            public string ImageFile { get; set; }
+            public int PersonID { get; set; }
+        }
 
         public CameraForm()
         {
@@ -25,7 +44,17 @@ namespace FacialRecognitionProject
 
         private void CameraForm_Load(object sender, EventArgs e)
         {
-            
+            using (StreamReader r = new StreamReader("C:/Users/Alex/Desktop/COMP3000/DatabasePeople.json"))
+            {
+                string json = r.ReadToEnd();
+                DBPeople = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+
+            using (StreamReader r2 = new StreamReader("C:/Users/Alex/Desktop/COMP3000/DatabaseImages.json"))
+            {
+                string json = r2.ReadToEnd();
+                DBImages = JsonConvert.DeserializeObject<List<Images>>(json);
+            }
         }
 
         private async void button1_Click(object sender, EventArgs e)
